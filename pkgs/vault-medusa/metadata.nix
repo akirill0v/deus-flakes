867fa357061f12c9087a75bd752fbc48676a0fe9
Aleksandr Kirillov <saratovsource@gmail.com>
2021-10-29 21:59:29 +0400

    Update metadata

--- pkgs/vault-medusa/metadata.nix
+++ pkgs/vault-medusa/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/jonasvinther/medusa";
   branch = "master";
   rev = "ce4bab95f74c07e886bcd9d8cd5cb795d1e6e903";
-  sha256 = "sha256-537rtqAKQ7og3bLs+ViWXSXGkMXsjeJk5iQ1h1P+Kdw=";
-  vendorSha256 = "1879j77k96684wi554rkjxydrj8g3hpp0kvxz03sd8dmwr3lh83j";
+  sha256 = "sha256-qPCFoYKzBqzU9x+VRVXN/EkjZm3LndW2xwPbUTIxi6Y=";
+  vendorSha256 = "sha256-5fqf4cGSE5iSbpy9DV1G9g2yrEynD2tQA+SQg8ypKAw=";
 }
